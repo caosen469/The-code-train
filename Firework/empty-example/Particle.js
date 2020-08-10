7adb5f194b2745b02232b197280202cdade3d3a7
Sihan Cao <sihancao496@gmail.com>
2020-08-09 20:51:42 -0700

    add the initial version of fadding effect

--- Firework/empty-example/Particle.js
+++ Firework/empty-example/Particle.js
@@ -1,13 +1,14 @@
 function Particle(x, y, firework) {
     this.pos = createVector(x, y);
-    // this.firework = !exploder;
+    this.firework = firework;
+    this.lifespan = 255;
 
-    if (firework){
+    if (this.firework){
         this.vel = createVector(0, random(-12, -8));
     }
     else{
         this.vel = p5.Vector.random2D();
-        //this.vel = this.vel.mult(random(1, 6));
+        this.vel = this.vel.mult(random(1, 6));
     }
     
     this.acc = createVector(0, 0);
@@ -17,9 +18,10 @@ function Particle(x, y, firework) {
     }
 
     this.update = function(){
-        // if (!this.firework){
-        //     this.vel.mult(0.85);
-        // }
+        if (!this.firework){
+            this.vel.mult(0.85);
+            this.lifespan -= 4;
+        }
         this.vel.add(this.acc);
         this.pos.add(this.vel); 
         this.acc.mult(0);
@@ -31,6 +33,14 @@ function Particle(x, y, firework) {
     }
     this.show = function(){
         //point(width/2, height/2);
-        point(this.pos.x, this.pos.y);     
+        if (!this.firework){
+            stroke(255, this.lifespan);
+            strokeWeight(2);
+        }
+        else{     
+            stroke(255);
+            strokeWeight(4);
+        } 
+        point(this.pos.x, this.pos.y);
     }
 }
