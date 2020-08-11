38e4d28c2ad7af53fe7fb7e3496e0d229af6a54e
Sihan Cao <sihancao496@gmail.com>
2020-08-11 15:17:40 -0700

    add the special function 'then' to see what is the 'promise' in the tensorflow,js

--- Tensorflow.js/empty-example/sketch.js
+++ Tensorflow.js/empty-example/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
   // put setup code here
   noCanvas;
-  tf.tensor([1, 2, 3, 4]).print();
+  // tf.tensor([1, 2, 3, 4]).print();
+  const values = [];
+  for (let i = 0; i < 30; i++){
+    values[i] = random(0, 100);
+  }
+  const shape = [2, 5, 3];
+  const tense = tf.tensor(values, shape, 'int32');
+
+
+  // const data = tf.tensor([1, 2, 3, 4, 1, 2, 3, 4], [2, 2, 2]);
+  // console.log(data.toString());
+  // console.log(data);
+  tense.data().then(function(stuff){
+    console.log(stuff);
+  })
 }
 
 function draw() {
